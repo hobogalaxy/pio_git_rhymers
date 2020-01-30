a591dba7d005d6cac7041bec667706609b8cbe75
hobogalaxy <zalewski.ukas@gmail.com>
2020-01-30 11:37:57 +0100

    1.5.6 - add documentation for DefaultRhymersFactory class

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -6,24 +6,39 @@ import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
+	/**
+	 * Default class for accessing different Rhymers.
+	 */
 
 	@Override
 	public defaultCountingOutRhymer GetStandardRhymer() {
+		/**
+		 * @return Default counting out rhymer.
+		 */
 		return new defaultCountingOutRhymer();
 	}
 
 	@Override
 	public defaultCountingOutRhymer GetFalseRhymer() {
+		/**
+		 * @return Default counting out rhymer.
+		 */
 		return new defaultCountingOutRhymer();
 	}
 
 	@Override
 	public defaultCountingOutRhymer GetFIFORhymer() {
+		/**
+		 * @return FIFO rhymer.
+		 */
 		return new FIFORhymer();
 	}
 
 	@Override
 	public defaultCountingOutRhymer GetHanoiRhymer() {
+		/**
+		 * @return Hanoi rhymer.
+		 */
 		return new HanoiRhymer();
 	}
 
